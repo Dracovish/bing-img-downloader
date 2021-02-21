f3f34f8589e681306ae79b1270af98bff318bda8
李铭昕 <715557344@qq.com>
2021-02-21 18:49:38 +0800

    Added show:name

---
+++ app/Command/ShowNameCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Command;
+
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as HyperfCommand;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Command
+ */
+class ShowNameCommand extends HyperfCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct('show:name');
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this->setDescription('Show skeleton name.');
+    }
+
+    public function handle()
+    {
+        $this->line(env('APP_NAME'), 'info');
+    }
+}
